7b2348fbc4515fd018783a7bb7eff8cab44024a9
Naoya Kohara <86583481+naonaonao708@users.noreply.github.com>
2023-01-03 03:29:21 +0000

    Add pub code

--- src/lib.rs
+++ src/lib.rs
@@ -5,6 +5,7 @@ mod front_of_house {
         fn seat_at_table() {}
     }
 
+
     mod serving {
         fn take_order() {}
 
@@ -12,4 +13,10 @@ mod front_of_house {
 
         fn take_payment() {}
     }
+}
+
+pub fn eat_at_restaurant() {
+    crate::front_of_house::hosting::add_to_waitlist();
+
+    front_of_house::hosting::add_to_waitlist();
 }
