8f4325df7e999b7c45880d282152ccd21ed1493f
naonao708 <naonao708@outlook.com>
2023-01-03 12:07:20 +0000

    finish 7chap

--- src/lib.rs
+++ src/lib.rs
@@ -1,10 +1,15 @@
 mod front_of_house {
-    mod hosting {
-        fn add_to_waitlist() {}
+    pub mod hosting {
+        pub fn add_to_waitlist() {}
 
-        fn seat_at_table() {}
+        //fn seat_at_table() {}
     }
 
+pub fn eat_at_restaurant() {
+    crate::front_of_house::hosting::add_to_waitlist();
+
+    front_of_house::hosting::add_to_waitlist();
+}
 
     mod serving {
         fn take_order() {}
@@ -15,8 +20,35 @@ mod front_of_house {
     }
 }
 
-pub fn eat_at_restaurant() {
-    crate::front_of_house::hosting::add_to_waitlist();
+use crate::front_of_house::hosting;
 
-    front_of_house::hosting::add_to_waitlist();
+pub fn eat_at_restaurant() {
+    hosting::add_to_waitlist();
+    hosting::add_to_waitlist();
+    hosting::add_to_waitlist();
+}
+
+fn serve_order() {}
+
+mod back_of_house {
+    pub struct Breakfast {
+        pub toast: String,
+        seasonal_fruit: String,
+    }
+
+    impl Breakfast {
+        pub fn summer(toast: &str) -> Breakfast {
+            Breakfast {
+                toast: String::from(toast),
+                seasonal_fruit: String::from("peaches")
+            }
+        }
+    }
+}
+
+pub fn eat_at_restaurant() {
+    let mut meal = back_of_house::Breakfast::summer("Rye");
+
+    meal.toast = String::from("Wheat");
+    println!("I'd like {} toast please", meal.toast);
 }
